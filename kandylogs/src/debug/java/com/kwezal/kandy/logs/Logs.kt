b59e48455343b98d02700b79e4752959de1e77ab
KajetanSuchanski <KajetanSuchanski@gmail.com>
2020-11-13 15:14:16 +0100

    Logs update - Lazy custom tags - Throwable parameter made lazy - Log tag generation improvement

--- kandylogs/src/debug/java/com/kwezal/kandy/logs/Logs.kt
+++ kandylogs/src/debug/java/com/kwezal/kandy/logs/Logs.kt
@@ -4,64 +4,96 @@ package com.kwezal.kandy.logs
 
 import android.util.Log
 
-inline fun Any.logV(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.v(tag, msg())
+inline fun Any.logV(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.v(customTag() ?: tag, msg())
     } else {
-        Log.v(tag, msg(), tr)
+        Log.v(customTag() ?: tag, msg(), throwable)
     }
 }
 
-inline fun Any.logD(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.d(tag, msg())
+inline fun Any.logD(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.d(customTag() ?: tag, msg())
     } else {
-        Log.d(tag, msg(), tr)
+        Log.d(customTag() ?: tag, msg(), throwable)
     }
 }
 
-inline fun Any.logI(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.i(tag, msg())
+inline fun Any.logI(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.i(customTag() ?: tag, msg())
     } else {
-        Log.i(tag, msg(), tr)
+        Log.i(customTag() ?: tag, msg(), throwable)
     }
 }
 
-inline fun Any.logW(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.w(tag, msg())
+inline fun Any.logW(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.w(customTag() ?: tag, msg())
     } else {
-        Log.w(tag, msg(), tr)
+        Log.w(customTag() ?: tag, msg(), throwable)
     }
 }
 
-inline fun Any.logE(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.e(tag, msg())
+inline fun Any.logE(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.e(customTag() ?: tag, msg())
     } else {
-        Log.e(tag, msg(), tr)
+        Log.e(customTag() ?: tag, msg(), throwable)
     }
 }
 
-inline fun Any.logWtf(tr: Throwable? = null, msg: () -> String) {
-    if (tr == null) {
-        Log.wtf(tag, msg())
+inline fun Any.logWtf(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+    val throwable = tr()
+    if (throwable == null) {
+        Log.wtf(customTag() ?: tag, msg())
     } else {
-        Log.wtf(tag, msg(), tr)
+        Log.wtf(customTag() ?: tag, msg(), throwable)
     }
 }
+
+private const val TAG_MAX_LENGTH = 23
 
 val Any.tag: String
     get() {
-        val simpleName = javaClass.simpleName
-        return if (simpleName.isNotEmpty())
-            simpleName
-        else {
-            val name = javaClass.name.substringAfterLast('.')
-            if (name.isNotEmpty())
+        val name = javaClass.name.substringAfterLast('.')
+        return if (name.isNotEmpty()) {
+            if (name.length > TAG_MAX_LENGTH) {
+                "…${name.substring(name.length - TAG_MAX_LENGTH + 1)}"
+            } else {
                 name
-            else
-                "Unknown context"
+            }.replace("$", ".")
+        } else {
+            "Unknown context"
         }
     }
--- kandylogs/src/release/java/com/kwezal/kandy/logs/Logs.kt
+++ kandylogs/src/release/java/com/kwezal/kandy/logs/Logs.kt
@@ -2,11 +2,46 @@
 
 package com.kwezal.kandy.logs
 
-inline fun Any.logV(tr: Throwable? = null, msg: () -> String) {}
-inline fun Any.logD(tr: Throwable? = null, msg: () -> String) {}
-inline fun Any.logI(tr: Throwable? = null, msg: () -> String) {}
-inline fun Any.logW(tr: Throwable? = null, msg: () -> String) {}
-inline fun Any.logE(tr: Throwable? = null, msg: () -> String) {}
-inline fun Any.logWtf(tr: Throwable? = null, msg: () -> String) {}
+inline fun Any.logV(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
+
+inline fun Any.logD(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
+
+inline fun Any.logI(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
+
+inline fun Any.logW(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
+
+inline fun Any.logE(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
+
+inline fun Any.logWtf(
+    customTag: () -> String? = { null },
+    tr: () -> Throwable? = { null },
+    msg: () -> String
+) {
+}
 
 inline val Any.tag get() = ""
